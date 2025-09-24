e1d062f83ee1c4d3c8becbc4a6886a614172d056
Ramakant Saini <ramakantsaini7565@gmail.com>
2025-09-24 14:34:08 +0530

    Configure Database into EmployeeDaoImpl java file

--- src/main/java/com/explore/Main.java
+++ src/main/java/com/explore/Main.java
@@ -1,33 +1,19 @@
 package com.explore;
 
-//import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
+
+import com.explore.dao.EmployeeDao;
+import com.explore.dao.EmployeeDaoImpl;
+import com.explore.entity.Employee;
 //import java.sql.Statement;
 
 public class Main {
-
-	private static String url = "jdbc:mysql://localhost:3306/db1";
-	private static String username = "root";
-	private static String password = "admin@123";
-
-
-	public static void main(String[] args) {
-		try {
-
-//			conn = DriverManager.getConnection(url, username, password);
-//
-//			st = conn.createStatement();
-//
-//			st.executeUpdate("INSERT INTO ducatstudents(id,name,salary) values (1,'Aman Saini',20000)");
-			
-			DriverManager.getConnection(url, username, password).createStatement().executeUpdate("INSERT INTO ducatstudents(id,name,salary) values (2,'Raj Saini',30000)");
-
-			System.out.println("Data Inserted successfully");
-
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
+	public static void main(String[] args) throws SQLException {
+		
+		EmployeeDao edao = new EmployeeDaoImpl();
+		Employee e1 = new Employee(4,"Piyush Saini",200000);
+		
+		edao.InsertData(e1);
 	}
 
 }
